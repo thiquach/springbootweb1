6c01e32a7787afd72a30fdcc25a2d80f70293d9e
Thi Quach <amylyquach@gmail.com>
2025-12-31 10:44:23 +1100

    add display result using @RequestParam

--- src/main/java/com/spring/SpringBootWeb1/HomeController.java
+++ src/main/java/com/spring/SpringBootWeb1/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,10 +15,8 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session) {
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session) {
 
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
         session.setAttribute("result", result);
         return "result.jsp";
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -4,7 +4,7 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h2> Result is : <%= session.getAttribute("result") %></h2>
+<h2> Result is : ${result} </h2>
 
 
 </body>
