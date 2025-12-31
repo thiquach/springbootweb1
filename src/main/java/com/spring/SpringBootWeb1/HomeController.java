e4b4a40e082cf9a9524e3698b6c217acbf8346e9
Thi Quach <amylyquach@gmail.com>
2025-12-31 11:16:44 +1100

    add model and view

--- src/main/java/com/spring/SpringBootWeb1/HomeController.java
+++ src/main/java/com/spring/SpringBootWeb1/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -17,10 +18,11 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv) {
 
         int result = num1 + num2;
-        model.addAttribute("result", result);
-        return "result";
+        mv.addObject("result", result);
+        mv.setViewName("result");
+        return mv;
     }
 }
